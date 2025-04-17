47dddea848796b646ebeeb1ebef704b7e727cf0e
Mathias Celle <[mathias.celle42230@gmail.com]>
2025-04-17 18:02:33 +0200

    correction

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
 <?php
+require_once 'classes/Livre.php';
+require_once 'classes/Roman.php';
 require_once 'classes/Bibliotheque.php';
 
 $biblio = new Bibliotheque('config/database.php');
-$biblio->chargerLivres();
 
-// Créer des livres
-$roman = new Roman(null, "Le Petit Prince", "Antoine de Saint-Exupéry", 1943, "Philosophie", 96);
-$bd = new BandeDessinee(null, "Astérix chez les Bretons", "René Goscinny", 1966, "Albert Uderzo", "Astérix", 8);
+// Crée un roman
+$roman = new Roman("L'Étranger", "Albert Camus", 1942, "Philosophique", 123);
 
-// Ajouter à la bibliothèque
+// Ajoute le roman
 $biblio->ajouterLivre($roman);
-$biblio->ajouterLivre($bd);
-
-// Afficher infos
-echo $roman->getInfos() . "<br>";
-echo $bd->getInfos() . "<br>";
-
-// Vérifier série
-echo $bd->estDansLaMemeSerieQue($bd) ? "Même série<br>" : "Série différente<br>";
-
-// Emprunter
-$biblio->emprunterLivre(1);
-$biblio->retournerLivre(1);
-
-// Lister livres disponibles
-echo "<h3>Livres disponibles :</h3>";
-foreach ($biblio->getLivresDisponibles() as $livre) {
-    echo $livre->getInfos() . "<br>";
-}
-?>
